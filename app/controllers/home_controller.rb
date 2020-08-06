c8fcedd1d0f6ea3b59545c5b37fad44d0bcc7d4a
Alexandre Luiz Elias <alexandre.alpe@gmail.com>
2020-08-06 16:18:59 -0300

    Banco criado, mas ainda não inseri nada nele através do console do rails, ou seja, meu controller ainda não está lendo do banco

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,12 @@ class HomeController < ApplicationController
   def index
     @recipes = []
     @recipes << Recipe.new(title: 'Bolo', cuisine: 'Brasileira',
-                           recipe_type: 'Sobremesa', cook_time: 30)
-    @recipes << Recipe.new(title: 'Bolo de cenoura', cuisine: 'Brasileira',
-                           recipe_type: 'Sobremesa', cook_time: 30)
+                           recipe_type: 'Sobremesa', cook_time: 30, 
+                           ingredients: 'Farinha', 'ovo','leite', 'açucar',
+                           cook_method: 'Mistura tudo e leve ao forno')
+    @recipes << Recipe.new(title: 'Pudim', cuisine: 'Brasileira',
+                           recipe_type: 'Sobremesa', cook_time: 90,
+                           ingredients: 'Farinha', 'ovo','leite condensado',
+                           cook_method: 'Mistura tudo e leve ao forno')
   end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,13 +1,2 @@
-class Recipe
-  # Lembrando que keyword arguments permite que eu crie objetos (.new) em qualquer ordem e não na ordem específica.
-  attr_accessor :title, :cuisine, :recipe_type, :cook_time
-  
-  # title, cuisine...são os atributos da classe recipe
-  # A cada .new que eu dou, crio uma instância da classe recipe, que é um objeto.
-  def initialize(title:, cuisine:, recipe_type:, cook_time:)
-    @title = title
-    @cuisine = cuisine
-    @recipe_type = recipe_type
-    @cook_time = cook_time
-  end
+class Recipe < ApplicationRecord
 end
